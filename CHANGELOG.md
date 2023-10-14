9e75a8f34dc88c92581bb530743647c86a95fe82
walker84837 <walker84837@gmail.com>
2023-10-14 20:04:30 +0200

    Add project files, and add StructOpt CLI args parsing

---
+++ CHANGELOG.md
@@ -0,0 +1,45 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/),
+and this project adheres to [Semantic Versioning](https://semver.org/).
+
+## \[Unreleased\]
+
+## \[0.0.2\] - 2023-10-15
+
+### Added
+
+  - Instead of using manual user input, the program will instead take them from command-line arguments, handled by StructOpt.
+  - Added project files:
+      - CHANGELOG.md
+      - CODE\_OF\_CONDUCT.md
+
+### Fixed
+
+  - Renamed LICENSE to LICENSE.md to reflect the right formatting.
+
+### Changed
+
+  - None.
+
+### Removed
+
+## \[0.0.1\] - 2023-07-31
+
+### Added
+
+  - Initial release of `wifi-qrcode-rs`.
+
+### Fixed
+
+  - None.
+
+### Changed
+
+  - None.
+
+### Removed
+
+  - None.
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,39 @@
-use std::io::Write;
-use qrcode::{QrCode, EcLevel, render::svg::Color};
-use qrcode::types::QrError;
+use qrcode::{render::svg::Color, types::QrError, EcLevel, QrCode};
 use std::fs::File;
-use std::io;
+use std::io::Write;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short, long)]
+    ssid: String,
+
+    #[structopt(short, long)]
+    password: String,
+
+    #[structopt(short, long)]
+    encryption: String,
+}
 
 fn generate_qr_code(wifi_info: &str) -> Result<QrCode, QrError> {
-    QrCode::with_error_correction_level(wifi_info.as_bytes(), EcLevel::H)
-        .map_err(|e| qrcode::types::QrError::from(e))
+    QrCode::with_error_correction_level(wifi_info.as_bytes(), EcLevel::H).map_err(|e| QrError::from(e))
 }
 
 fn main() {
-    let mut ssid = String::new();
-    let mut passwd = String::new();
-    let mut encrypt = String::new();
-
-    println!("SSID (Wi-Fi network name):");
-    io::stdin()
-        .read_line(&mut ssid)
-        .expect("Failed to read SSID");
-    println!("Wi-Fi network password:");
-    io::stdin()
-        .read_line(&mut passwd)
-        .expect("Failed to read password");
-    println!("Wi-Fi encryption (WPA|WEP|nopass):");
-    io::stdin()
-        .read_line(&mut encrypt)
-        .expect("Failed to read encryption");
+    let options = Options::from_args();
 
     let wifi_info = format!(
         "WIFI:T:{};S:{};P:{};;",
-        encrypt.trim(),
-        ssid.trim(),
-        passwd.trim()
+        options.encryption,
+        options.ssid,
+        options.password
     );
 
     match generate_qr_code(&wifi_info) {
         Ok(qr_code) => {
             let renderer: String = qr_code.render::<Color>().build();
             let svg_data: String = renderer.to_string();
+
             match File::create("qrcode.svg") {
                 Ok(mut f) => {
                     f.write_all(svg_data.as_bytes())
@@ -55,3 +51,4 @@ fn main() {
         }
     }
 }
+
